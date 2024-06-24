a73fe8e9ec50a9f53fc76f01415b06feaa895854
Matheusfr34 <matheusfr34@icloud.com>
2024-06-24 14:58:31 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
 server.use(router)
-server.listen(3000, () => {
+server.listen(3001, () => {
   console.log('JSON Server is running')
 })
