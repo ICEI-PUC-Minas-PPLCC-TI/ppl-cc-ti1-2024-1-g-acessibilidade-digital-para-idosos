fc4e365223aa57648b2055f8e6343be581683a6e
Vitor Tiradentes <vitortss33@gmail.com>
2024-06-30 15:27:49 -0300

    resolvendo

--- docs/pages/index.js
+++ docs/pages/index.js
@@ -6,11 +6,22 @@ const port = 3001;
 
 app.use(express.json());
 
+<<<<<<< Updated upstream
 const dbPath = path.join(__dirname, 'db.json');
 let db = JSON.parse(fs.readFileSync(dbPath));
 
 app.use(express.static(path.join(__dirname, 'docs/pages')));
 
+=======
+// Carregar db.json da raiz do projeto
+const dbPath = path.join(__dirname, 'db.json');
+let db = JSON.parse(fs.readFileSync(dbPath));
+
+// Middleware para servir arquivos estáticos
+app.use(express.static(path.join(__dirname, 'docs/pages')));
+
+// Obter todos os favoritos
+>>>>>>> Stashed changes
 app.get('/favoritos', (req, res) => {
     const favoritos = db.favoritos.map(fav => {
         const topico = db.topicos.find(top => top.id === fav.topicoId);
@@ -19,6 +30,10 @@ app.get('/favoritos', (req, res) => {
     res.json(favoritos);
 });
 
+<<<<<<< Updated upstream
+=======
+// Adicionar aos favoritos
+>>>>>>> Stashed changes
 app.post('/favoritos', (req, res) => {
     const { UserId, topico } = req.body;
 
@@ -41,7 +56,11 @@ app.post('/favoritos', (req, res) => {
     res.status(201).json(novoFavorito);
 });
 
+<<<<<<< Updated upstream
 
+=======
+// Iniciar o servidor
+>>>>>>> Stashed changes
 app.listen(port, () => {
     console.log(`Servidor rodando em http://localhost:${port}`);
 });
